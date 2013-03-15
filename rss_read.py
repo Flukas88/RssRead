295ccf4bc880abb0170e28736b5c05fbe9d67898
Luca Francesca <luca.francesca01@gmail.com>
2013-03-15 12:08:32 +0100

    Update rss_read.py
    Refactoring in class

--- rss_read.py
+++ rss_read.py
@@ -15,56 +15,66 @@
 #    along with this program.
 #    If not, see <http://www.gnu.org/copyleft/lesser.html>.
 #
+#  RssRead 0.1
 #  by Luca Francesca, 2013
 
-from __future__ import print_function
-import sys
 import feedparser
 import xml.etree.cElementTree as etree
 
 
-siteConf = {}
+class RssRead:
+    """RssRead main method"""
+    def __init__(self):
+        self.siteConf = {}
+        self.NewsDict = {}
 
+    """Configuration loading method"""
+    def loadConf(self):
+        self.tree = etree.parse('config.xml')
+        self.Config = self.tree.getroot()
+        for child in self.Config:
+            self.siteConf[child.find('name').text] = child.find('url').text
 
-def loadConf():
-    tree = etree.parse('config.xml')
-    root = tree.getroot()
-    for child in root:
-        siteConf[child.find('name').text] = child.find('url').text
+    """News loading method"""
+    def loadNewsRss(self, site):
+        self.feed = feedparser.parse(self.siteConf[site],
+                                     agent='RssRead/0.1 +http://ciscoland.eu/')
+        for news in self.feed.entries:
+            self.NewsDict[news.title.encode('utf-8')] = news.link
 
+    """News getting method"""
+    def getNews(self):
+        pass
 
-def loadNewsRss(site):
-        d = feedparser.parse(siteConf[site], agent='RssRead/0.1 +http://ciscoland.eu/')
-        for news in d.entries:
-            print (news.title.encode('utf-8'), '\n', '[ fonte: ', news.link, ']\n -- --\n')
+    """Configuration site adding  method"""
+    def addSite(self, name, url):
+        Site = self.tree.SubElement(self.Config, 'site')
+        self.tree.SubElement(Site, 'name').text = name
+        self.tree.SubElement(Site, 'url').text = url
+        self.tree.write('config.xml', encoding='utf-8')
 
+    """Configuration site removing  method"""
+    def removeSite(self, site):
+        for Site in self.Config.findall('site'):
+            if Site.find('name').text == site:
+                self.Config.remove(Site)
+        self.tree.write('config.xml', encoding='utf-8')
 
-def addSite(name, url):
-    root = etree.parse('config.xml')
-    Config = root.getroot()
-    Site = etree.SubElement(Config, 'site')
-    etree.SubElement(Site, 'name').text = name
-    etree.SubElement(Site, 'url').text = url
-    root.write('config.xml', encoding='utf-8')
-
-
-def removeSite(site):
-    root = etree.parse('config.xml')
-    Config = root.getroot()
-    for Site in Config.findall('site'):
-        if Site.find('name').text == site:
-            Config.remove(Site)
-    root.write('config.xml', encoding='utf-8')
+# Testing
 
 
 def main():
-    loadConf()
+    rss = RssRead()
+    rss.loadConf()
+    rss.addSite('io', 'tu')  # problema
     try:
-        loadNewsRss(sys.argv[1])
+        pass
+        # rss.loadNewsRss('Ansa')
+        # print rss.NewsDict
     except IndexError:
         print ('Non hai definito nessuna sorgente RSS!!\n \
                 ====== Siti validi ====== ')
-        for arg in siteConf:
+        for arg in rss.siteConf:
             print (arg, '\n+-+-+')
     except KeyError:
         print ('Nome sito non valido!')
