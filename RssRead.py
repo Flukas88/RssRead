47c1ced43b2b62dfdaf724bb5dc55d53a04f4f7e
Luca Francesca <luca.francesca01@gmail.com>
2013-03-15 19:15:21 +0100

    Update and rename rss_read.py to RssRead.py
    More pythonic

--- RssRead.py
+++ RssRead.py
@@ -1,22 +1,22 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU LESSER GENERAL PUBLIC LICENSEG as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU LESSER GENERAL PUBLIC LICENSEG as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
 #
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
 #
-#    You should have received a copy of the GNU General Public License
-#    along with this program.
-#    If not, see <http://www.gnu.org/copyleft/lesser.html>.
+# You should have received a copy of the GNU General Public License
+# along with this program.
+# If not, see <http://www.gnu.org/copyleft/lesser.html>.
 #
-#  RssRead 0.1
-#  by Luca Francesca, 2013
+# RssRead 0.11
+# by Luca Francesca, 2013
 
 import feedparser
 import xml.etree.cElementTree as etree
@@ -33,16 +33,17 @@ class RssRead:
         self.Config = self.tree.getroot()
 
     def loadConf(self):
-        """Configuration loading method"""
+        """Configuration loading method (first call of the class)"""
         for child in self.Config:
-            self.siteConf[child.find('name').text] = child.find('url').text
+            self.siteConf = dict((child.find('name').text, child.find('url').text)
+                                 for child in self.Config)
 
     def loadNewsRss(self, site):
-        """News loading method"""
+        """News loading method and formatting it in xhtml.\nIt *must* be followed by getNews() """
         self.feed = feedparser.parse(self.siteConf[site],
-                                     agent='RssRead/0.1 +http://ciscoland.eu/')
+                                     agent='RssRead/0.11 +http://ciscoland.eu/')
         for news in self.feed.entries:
-            newsStr = '<b><a href="' + news.link + '">' + news.title + '</a></b><br />'
+            newsStr = '<a href="' + news.link + '">' + news.title + '</a><br />'
             newsStr = newsStr.encode('utf-8')
             self.news.append(newsStr)
 
@@ -58,21 +59,19 @@ class RssRead:
         self.tree.write(self.fileName, encoding='utf-8')
 
     def removeSite(self, site):
-        """Configuration site removing  method"""
+        """Configuration site removing method"""
         for Site in self.Config.findall('site'):
             if Site.find('name').text == site:
                 self.Config.remove(Site)
         self.tree.write(self.fileName, encoding='utf-8')
 
-# Testing
-
 
 def main():
     rss = RssRead()
     rss.loadConf()
-    rss.loadNewsRss('Ansa')
-    for new in rss.getNews('Ansa'):
-        print new
+    rss.loadNewsRss('slashdot')
+    for new in rss.getNews('slashdot'):
+        print new, '\n'
 
 
 if __name__ == '__main__':
