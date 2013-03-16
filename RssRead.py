20eaffbcfeb504b843e51a0719d0b330433f5843
Luca Francesca <luca.francesca01@gmail.com>
2013-03-16 15:40:18 +0100

    Update RssRead.py

--- RssRead.py
+++ RssRead.py
@@ -37,7 +37,7 @@ class RssRead:
                               for child in self._Config)
 
     def loadNewsRss(self, site):
-        """It *must* be followed by getNews(). Args are site."""
+        """Load the news. You have to specify the site."""
         self.feed = feedparser.parse(self._siteConf[site],
                                      agent='RssRead/0.11 +http://ciscoland.eu/')
         self._news = list('<a href="' + news.link.encode('utf-8') + '">' + news.title.encode('utf-8') + '</a><br />'
@@ -45,8 +45,6 @@ class RssRead:
 
     @property
     def News(self):
-        """News getting method.
-        """
         return self._news
 
     def _addSite(self, name, url):
