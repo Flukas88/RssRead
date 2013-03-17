677e88b8e356fefe704514018b42f2845b067f07
Luca Francesca <luca.francesca01@gmail.com>
2013-03-17 21:07:52 +0100

    Update RegressionTest.py
    Added test for add/remove function

--- RegressionTest.py
+++ RegressionTest.py
@@ -14,8 +14,28 @@ class RegressionTest(unittest.TestCase):
         self.assertNotEqual(self.rss._siteConf, {}, 'Problems with configuration loading')
 
     def test_loading(self):
-        self.rss.loadNewsRss('slashdot')
+        self.rss.loadNewsRss('torrent')
         self.assertNotEqual(self.rss.News, [], 'Problems with rss loading')
+
+    def test_unicode_except(self):
+        try:
+            self.rss.loadNewsRss('torrent')
+        except UnicodeEncodeError:
+            self.fail('Unicode Error thrown')
+
+    def test_add_site(self):
+        try:
+            self.rss.loadNewsRss('torrent')
+            self.rss += 'io', 'tu'
+        except TypeError:
+            self.fail('Already present site exception thrown')
+
+    def test_remove_site(self):
+        try:
+            self.rss.loadNewsRss('torrent')
+            self.rss -= 'io'
+        except TypeError:
+            self.fail('Already removed site exception thrown')
 
 
 if __name__ == '__main__':
