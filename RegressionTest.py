03f3d70a05d5e31d48adcab58f7f9ed5138ac4f4
Luca Francesca <luca.francesca01@gmail.com>
2013-03-17 21:19:02 +0100

    Update RegressionTest.py

--- RegressionTest.py
+++ RegressionTest.py
@@ -18,60 +18,42 @@
 # RssRead 0.2
 # by Luca Francesca, 2013
 
-import feedparser
-import xml.etree.cElementTree as etree
+import RssRead as feed
+import unittest
 
 
-class RssRead:
-    """ Default config.xml or pass it a customized one as parameter. """
-    def __init__(self, fileName='config.xml'):
-        self._fileName = fileName
-        self._siteConf = {}
-        self._news = []
-        self._tree = etree.parse(self._fileName)
-        self._Config = self._tree.getroot()
-        self._loadConf()
+class RegressionTest(unittest.TestCase):
 
-    def _loadConf(self):
-        """Configuration loading method"""
-        self._siteConf = dict((child.find('name').text, child.find('url').text)
-                              for child in self._Config)
+    def setUp(self):
+        self.rss = feed.RssRead()
 
-    def loadNewsRss(self, site):
-        """Load the news. You have to specify the site."""
-        self.feed = feedparser.parse(self._siteConf[site],
-                                     agent='RssRead/0.2 +http://ciscoland.eu/')
-        self._news = list('<a href="' + news.link.encode('utf-8') + '">' + news.title.encode('utf-8') + '</a><br />'
-                          for news in self.feed.entries)
+    def test_configuration(self):
+        self.assertNotEqual(self.rss._siteConf, {}, 'Problems with configuration loading')
 
-    @property
-    def News(self):
-        return self._news
+    def test_loading(self):
+        self.rss.loadNewsRss('torrent')
+        self.assertNotEqual(self.rss.News, [], 'Problems with rss loading')
 
-    def _addSite(self, name, url):
-        """Configuration site adding method. The args are (name, url) """
-        if name in self._siteConf:
-            raise TypeError('Site already present')
-        else:
-            Site = etree.SubElement(self._Config, 'site')
-            etree.SubElement(Site, 'name').text = name
-            etree.SubElement(Site, 'url').text = url
-            self._tree.write(self._fileName, encoding='utf-8')
-            self._siteConf[name] = url
+    def test_unicode_except(self):
+        try:
+            self.rss.loadNewsRss('torrent')
+        except UnicodeEncodeError:
+            self.fail('Unicode Error thrown')
 
-    def _removeSite(self, site):
-        """Configuration site removing method. Just give it the site name """
-        if site not in self._siteConf:
-            raise TypeError('Site already removed')
-        else:
-            for Site in self._Config.findall('site'):
-                if Site.find('name').text == site:
-                    self._Config.remove(Site)
-            self._tree.write(self._fileName, encoding='utf-8')
-            del self._siteConf[site]
+    def test_add_site(self):
+        try:
+            self.rss.loadNewsRss('torrent')
+            self.rss += 'io', 'tu'
+        except TypeError:
+            self.fail('Already present site exception thrown')
 
-    def __isub__(self, site):
-        self._removeSite(site)
+    def test_remove_site(self):
+        try:
+            self.rss.loadNewsRss('torrent')
+            self.rss -= 'io'
+        except TypeError:
+            self.fail('Already removed site exception thrown')
 
-    def __iadd__(self, cnf):
-        self._addSite(cnf[0], cnf[1])
+
+if __name__ == '__main__':
+    unittest.main()
