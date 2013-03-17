cd9a9499ad17c8ca0f7df4edd5be4a1b97c489dc
Luca Francesca <luca.francesca01@gmail.com>
2013-03-17 20:07:26 +0100

    Update RegressionTest.py

--- RegressionTest.py
+++ RegressionTest.py
@@ -10,12 +10,13 @@ class RegressionTest(unittest.TestCase):
     def setUp(self):
         self.rss = feed.RssRead()
 
-    def test1f(self):
+    def test_configuration(self):
         self.assertNotEqual(self.rss._siteConf, {}, 'Problems with configuration loading')
 
-    def test2(self):
+    def test_loading(self):
         self.rss.loadNewsRss('slashdot')
         self.assertNotEqual(self.rss.News, [], 'Problems with rss loading')
+
 
 if __name__ == '__main__':
     unittest.main()
