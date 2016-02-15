5473404eb9629caf6e7ba4ed228e3bd42c3d473d
Luca Francesca <luca@lucafrancesca.me>
2016-02-15 12:36:48 +0000

    Fixed docs generation

--- VerifyConf.py
+++ VerifyConf.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU LESSER GENERAL PUBLIC LICENSEG as
 # published by the Free Software Foundation, either version 3 of the
